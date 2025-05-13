a08ab8a76c879d085438231a4b666f1a73519afd
wangmengdi06 <wangmengdi06@58.com>
2025-05-13 20:17:51 +0800

    add command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Subcommand, Parser, Args};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+
+    name: Option<String>,
+
+    #[command(subcommand)]
+    command: Option<Commands>
+}
+
+#[derive(Subcommand)]
+enum Commands  {
+    /// Serve the application
+    Serve(ServeArgs)
+}
+
+#[derive(Args)]
+struct ServeArgs {
+    /// The host to bind to for serving.
+    #[arg(short, long, default_value = "0.0.0.0")]
+    host: String,
+    /// The port to bind to for serving.
+    #[arg(short, long, default_value = "8080")]
+    port: u16
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
